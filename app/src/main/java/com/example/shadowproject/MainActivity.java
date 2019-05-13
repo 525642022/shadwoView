e27489a217e665edcf4def8f792879987d5d81f4
刘景昌 <liujingchang@yyhealth.com>
2019-05-13 15:43:44 +0800

    提交新工程

---
+++ app/src/main/java/com/example/shadowproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.shadowproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//一个android 阴影实现方式的demo
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
